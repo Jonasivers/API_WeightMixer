6ebe201a9bcc7d9163b3b2701e5fa3f2038cec6d
Jonasivers <jonas.iversminde@gmail.com>
2023-07-26 11:40:47 +0200

    Not tested

---
+++ API_WeightMixer.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from Driver import *
+
+
+app = FastAPI()
+
+class SpeedModel(BaseModel):
+    speed: int
+
+@app.get("/get_name")
+def get_name():
+    return {"name": get_Name()}
+
+@app.post("/set_speed")
+def set_speed(speed: SpeedModel):
+    return {"result": set_Speed(speed.speed)}
+
+@app.get("/get_speed")
+def get_speed():
+    return {"speed": get_Speed()}
+
+@app.get("/get_speed_set_point")
+def get_speed_set_point():
+    return {"speed_set_point": get_Speed_Set_Point()}
+
+@app.get("/get_viscosity_trend")
+def get_viscosity_trend():
+    return {"viscosity_trend": get_Viscosity_Trend()}
+
+@app.post("/start_mixer")
+def start_mixer():
+    return {"result": start_Mixer()}
+
+@app.post("/stop_mixer")
+def stop_mixer():
+    return {"result": stop_Mixer()}
+
+@app.post("/reset")
+def reset():
+    return {"result": reset()}
+
+@app.post("/start_weight")
+def start_weight():
+    return {"result": start_Weight()}
+
+@app.post("/stop_weight")
+def stop_weight():
+    return {"result": stop_Weight()}
+
+@app.get("/get_weight_value")
+def get_weight_value():
+    return {"weight_value": get_Weight_Value()}
+
+@app.get("/get_weight_status")
+def get_weight_status():
+    return {"weight_status": get_Weight_Status()}
---
+++ Driver.py
@@ -0,0 +1,154 @@
+import serial
+import time
+import os
+
+if os.name == 'nt':
+    comPort = "COM15"  # COM depends on computer, only for debugging
+else:
+    comPort = '/dev/ttyUSB0'
+
+weightMixer = serial.Serial(port=comPort, baudrate=9600, timeout=10, parity=serial.PARITY_EVEN, xonxoff=False)
+time.sleep(1)
+
+
+def get_Name() -> int:
+    weightMixer.write(bytes('IN_NAME \r \n', 'utf-8'))
+
+    getData = weightMixer.readline()
+    data = getData.decode('utf-8').rstrip()
+    print(data)
+
+    return 0
+
+
+def set_Speed(speed: int) -> int:
+    weightMixer.write(bytes('OUT_SP_4 ' + str(speed) + ' \r \n', 'utf-8'))
+    print('Setting speed to ' + str(speed))
+
+    return 0
+
+
+def get_Speed() -> int:
+    weightMixer.write(bytes('IN_PV_4 \r \n', 'utf-8'))
+
+    getData = weightMixer.readline()
+    data = getData.decode('utf-8').rstrip()
+    print(data)
+    speed = data[0:data.find(" ")]
+
+    return speed
+
+
+def get_Speed_Set_Point() -> int:
+    weightMixer.write(bytes('IN_SP_4 \r \n', 'utf-8'))
+
+    getData = weightMixer.readline()
+    data = getData.decode('utf-8').rstrip()
+    print(data)
+    speed = data[0:data.find(" ")]
+
+    return speed
+
+
+def get_Viscosity_Trend() -> int:
+    weightMixer.write(bytes('IN_PV_5 \r \n', 'utf-8'))
+
+    getData = weightMixer.readline()
+    data = getData.decode('utf-8').rstrip()
+    print(data)
+    viscosity = data[0:data.find(" ")]
+
+    return viscosity
+
+
+def start_Mixer() -> int:
+    weightMixer.write(bytes('START_4 \r \n', 'utf-8'))
+    print('Starting Mixer')
+
+    return 0
+
+
+def stop_Mixer() -> int:
+    weightMixer.write(bytes('STOP_4 \r \n', 'utf-8'))
+    print('Stopping Mixer')
+
+    return 0
+
+
+def reset() -> int:
+    weightMixer.write(bytes('RESET \r \n', 'utf-8'))
+    print('Resetting Mixer...')
+    time.sleep(30)
+
+    return 0
+
+
+def start_Weight() -> int:
+    weightMixer.write(bytes('START_90 \r \n', 'utf-8'))
+    print('Starting Weight')
+
+    return 0
+
+
+def stop_Weight() -> int:
+    weightMixer.write(bytes('STOP_90 \r \n', 'utf-8'))
+    print('Stopping Weight')
+
+    return 0
+
+
+def get_Weight_Value() -> int:
+    weightMixer.write(bytes('IN_PV_90 \r \n', 'utf-8'))
+
+    getData = weightMixer.readline()
+    data = getData.decode('utf-8').rstrip()
+    print(data)
+    weight = data[0:data.find(" ")]
+
+    return weight
+
+
+def get_Weight_Status() -> int:
+    tare = False
+    scale_on = False
+    scale_stable = False
+    scale_overload = False
+    scale_power_on = False
+
+    weightMixer.write(bytes('STATUS_90 \r \n', 'utf-8'))
+
+    getData = weightMixer.readline()
+    data = getData.decode('utf-8').rstrip()
+    print(data)
+    value = data[0:data.find(" ")]
+    value = int(value)
+    print(bin(value))
+
+    if value & 0b1:
+        print("Scale stable")
+        scale_stable = True
+    else:
+        print("Scale not stable")
+
+    if value & 0b1000:
+        print("Tare completed")
+        tare = True
+    else:
+        print("Tare not completed")
+
+    if value & 0b10000:
+        print("Scale on")
+        scale_on = True
+    else:
+        print("Scale not on")
+
+    if value & 0b1000000000:
+        print("Scale overload")
+        scale_overload = True
+
+    if value & 0b10000000000:
+        print("Scale power on")
+        scale_power_on = True
+
+    return tare, scale_on, scale_stable, scale_power_on, scale_overload
+
